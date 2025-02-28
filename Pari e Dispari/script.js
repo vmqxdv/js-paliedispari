ef3b8fe957a9d19c59b428a57a24873723317d21
vmqxdv <vquintilio@gmail.com>
2025-02-28 17:41:37 +0100

    add: Dichiarata funzione per generare numeri random in un range

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -23,6 +23,12 @@ function startNumGame(numType) {
       continue;
     };
 
+
   };
   
+};
+
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
