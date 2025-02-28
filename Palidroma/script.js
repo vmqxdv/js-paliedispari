876abc202e431d332fa2a846c94d624e7e370d12
vmqxdv <vquintilio@gmail.com>
2025-02-28 16:36:09 +0100

    feat: Dichiarata costante per l'input dell'utente

---
+++ Palidroma/script.js
@@ -0,0 +1,10 @@
+/*
+
+  Palidroma
+
+  Chiedere all’utente di inserire una parola
+  Creare una funzione per capire se la parola inserita è palindroma
+
+*/
+
+const userWord = prompt('Scrivi una parola per verificare che essa sia palindroma:');
